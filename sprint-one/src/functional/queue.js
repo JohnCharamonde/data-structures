56afd061b7b8dc1ab2b64b13d0b658281c9ffeeb
Jordan Heskett <jordanheskett@gmail.com>
2019-12-11 18:03:33 -0600

    Completed functional queue assignment :smile:

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -6,14 +6,36 @@ var Queue = function() {
 
   // Implement the methods below
 
+  var currentIndex = 0;
+  var firstIndex = 0;
   someInstance.enqueue = function(value) {
+    // add value to key at end of storage object
+
+
+
+    storage[currentIndex] = value;
+    currentIndex++;
+    // return storage
+    return storage;
   };
 
   someInstance.dequeue = function() {
+    // declare variable for output
+    var output = storage[firstIndex];
+    // remove key value pair from beginning of object
+    delete storage[firstIndex];
+    firstIndex++;
+    // return value that was removed
+    //debugger;
+    return output;
   };
 
   someInstance.size = function() {
+    // return size of storage object
+    return Object.keys(storage).length;
   };
 
   return someInstance;
 };
+
+
