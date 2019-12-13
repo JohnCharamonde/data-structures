0d5f03eb699b70728b95d450df73bdcefa732cdd
Jordan Heskett <jordanheskett@gmail.com>
2019-12-13 17:12:18 -0600

    Completed linkedList assignment :smile:

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,21 +1,45 @@
-var LinkedList = function() {
+var LinkedList = function () {
   var list = {};
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(value) {
-  };
+  list.addToTail = function (value) {
+    let temp;
+    if (list.head) {
+      temp = list.tail;
+    }
+    list.tail = Node(value);
+    if (temp) {
+      temp.next = list.tail;
+    } else {
+      list.head = list.tail;
+    }
+  };// Complexity is O(1)
 
-  list.removeHead = function() {
-  };
+  list.removeHead = function () {
+    if (list.head) {
+      let deleted = list.head;
+      delete list.head;
+      list.head = deleted.next;
+      return deleted.value;
+    }
+  };// Complexity is O(1)
 
-  list.contains = function(target) {
-  };
+  list.contains = function (target) {
+    // evaluate list.head.value against target
+    // evaluate list.head.next.value against target
+    for (currentNode = list.head; currentNode !== null; currentNode = currentNode.next) {
+      if (currentNode.value === target) {
+        return true;
+      }
+    }
+    return false;
+  };//Complexity is O(n)
 
   return list;
 };
 
-var Node = function(value) {
+var Node = function (value) {
   var node = {};
 
   node.value = value;
