434f1028a97c3c9a4c74a6f9b34c5b76288a0e58
Jordan Heskett <jordanheskett@gmail.com>
2019-12-12 18:41:41 -0600

    Completed es6 stack assignment :smile:

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,26 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.length = 0;
+
   }
 
+  push(value) {
+    this.storage[this.length] = value;
+    this.length++;
+    return this.length;
+  }
+
+  pop() {
+    if (this.length) {
+      this.deleted = this.storage[this.length - 1];
+      delete this.storage[this.length - 1];
+      this.length--;
+      return this.deleted;
+    }
+  }
+
+  size() {
+    return this.length;
+  }
 }
