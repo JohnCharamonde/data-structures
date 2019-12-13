0195960b3093eb30f46e6b7a3294184f015b1570
Jordan Heskett <jordanheskett@gmail.com>
2019-12-12 18:42:12 -0600

    Completed es6 queue assignment :smile:

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,27 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.currentIndex = 0;
+    this.firstIndex = 0;
+    this.length = 0;
+    this.storage = {};
   }
-
+  enqueue (value) {
+    this.storage[this.currentIndex] = value;
+    this.length++;
+    this.currentIndex++;
+    return this.length;
+  }
+  dequeue () {
+    if (this.length) {
+      this.deleted = this.storage[this.firstIndex];
+      delete this.storage[this.firstIndex];
+      this.firstIndex++;
+      this.length--;
+      return this.deleted;
+    }
+  }
+  size () {
+    return this.length;
+  }
 }
