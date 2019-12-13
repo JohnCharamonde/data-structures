506b3f360cb328e187a8185f4c624fc8dff1d213
Jordan Heskett <jordanheskett@gmail.com>
2019-12-12 18:38:04 -0600

    Completed prototypal stack assignment :smile:

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,29 @@
-var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Stack = function () {
+  var someInstance = Object.create(stackMethods);
+
+  someInstance.storage = {};
+  someInstance.length = 0;
+
+  return someInstance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function (value) {
+  this.storage[this.length] = value;
+  this.length++;
+  return this.storage;
+};
 
+stackMethods.pop = function () {
+  if (this.length) {
+    this.deleted = this.storage[this.length - 1];
+    delete this.storage[this.length - 1];
+    this.length--;
+    return this.deleted;
+  }
+};
+
+stackMethods.size = function () {
+  return this.length;
+};
