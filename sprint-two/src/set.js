ce5f8b761be3ae61aaecf245bed5e27862dabfbf
Jordan Heskett <jordanheskett@gmail.com>
2019-12-14 12:11:35 -0600

    Completed sprint-two set assignment :smile:

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,37 @@
-var Set = function() {
+var Set = function () {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
-setPrototype.add = function(item) {
+setPrototype.add = function (item) {
+  if (!this.contains(item)) {
+    this.storage.push(item);
+  }
 };
+// complexity O(n)
 
-setPrototype.contains = function(item) {
+setPrototype.contains = function (item) {
+  for (let i = 0; i < this.storage.length; i++) {
+    if (this.storage[i] === item) {
+      return true;
+    }
+  }
+  return false;
 };
+//compexity O(n)
 
-setPrototype.remove = function(item) {
+setPrototype.remove = function (item) {
+  for (let i = 0; i < this.storage.length; i++) {
+    if (this.storage[i] === item) {
+      this.storage.splice(i, 1);
+      break;
+    }
+  }
 };
-
+// complexity: O(n)
 /*
  * Complexity: What is the time complexity of the above functions?
  */
