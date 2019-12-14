7328a362c744c2eb034624beeb4427eefe1d6752
Jordan Heskett <jordanheskett@gmail.com>
2019-12-14 14:05:42 -0600

    Completed sprint-two hashTable assignment :smile:

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,26 +1,48 @@
-
-
-var HashTable = function() {
+var HashTable = function () {
   this._limit = 8;
+  this._size = 0;
   this._storage = LimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v) {
+HashTable.prototype.insert = function (k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index) || [];
+  for (var i = 0; i < bucket.length; i++) {
+    let tuple = bucket[i];
+    if (tuple[0] === k) {
+      var oldValue = tuple[1];
+      tuple[1] = v;
+      return oldValue;
+    }
+  }
+  bucket.push([k, v]);
+  this._size++;
+  this._storage.set(index, bucket);
 };
 
-HashTable.prototype.retrieve = function(k) {
+HashTable.prototype.retrieve = function (k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  for (let i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      return bucket[i][1];
+    }
+  }
 };
 
-HashTable.prototype.remove = function(k) {
+HashTable.prototype.remove = function (k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      var removed = bucket[i];
+      bucket.splice(i, 1);
+      this.size--;
+      return removed;
+    }
+  }
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
